9089e0cfa1592403ff719937f65f0b23f0adca12
Emmanuel-Develops <itakpeemma@gmail.com>
2024-06-28 11:56:57 +0100

    feat: setup tailwind, and build css

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  outDir: 'dist',
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
   external: ['react', 'react-dom'],
+  injectStyle: true,
 });
