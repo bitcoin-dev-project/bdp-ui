e7637d71616e1ee6d696f28aa8e5ccb34d2e65db
Emmanuel-Develops <itakpeemma@gmail.com>
2024-08-20 11:55:46 +0100

    ci: only include dist dir in assets

--- release.config.js
+++ release.config.js
@@ -3,11 +3,16 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
+    [
+      "@semantic-release/github",
+      {
+        assets: ["dist/**/*"],  // Include only the dist folder
+      },
+    ],
     [
       "@semantic-release/git",
       {
-        assets: ["dist/*"],
+        assets: ["dist/**/*"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
