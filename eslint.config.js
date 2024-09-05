3a8b7b9b2febd2262dd0ffb4cc469c8bedd03e2e
Emmanuel-Develops <itakpeemma@gmail.com>
2024-09-05 03:05:59 +0100

    ci: lint, prettier and typo check

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+const typescript = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const react = require('eslint-plugin-react');
+
+module.exports = [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': typescript,
+      'react': react,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    rules: {
+      ...typescript.configs['recommended'].rules,
+      ...react.configs['recommended'].rules,
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
