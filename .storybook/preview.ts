e37fec8cf00219d7824fdba7474c89ccb5b3e767
Emmanuel-Develops <itakpeemma@gmail.com>
2024-11-19 14:25:42 +0100

    chore: dev refactor from removing inject styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import "../src/styles/tailwind.output.css";
 
 const preview: Preview = {
   parameters: {
--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,8 @@ module.exports = [
       ...typescript.configs['recommended'].rules,
       ...react.configs['recommended'].rules,
       'no-console': ['warn', { allow: ['warn', 'error'] }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn'
     },
     settings: {
       react: {
